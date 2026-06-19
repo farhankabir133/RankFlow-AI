62cd8150d37d80e874936970e1f496e78a912b3c
Farhan Kabir <farhankabir133@gmail.com>
2026-06-19 17:39:07 +0600

    Updates

--- tmp_revert.cjs
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const output = execSync('git checkout -- src/components/ExamEngine.tsx', { encoding: 'utf-8' });
-  console.log('REVERT_SUCCESS:', output);
-} catch (error) {
-  console.error('REVERT_FAIL:', error.message);
-}
